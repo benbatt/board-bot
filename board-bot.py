001a0d3f490f31ae21c65006f80b51afd61cee99
Ben Batt <benbatt@gmail.com>
2026-01-28 22:57:11 +1100

    Add proof of concept script

---
+++ board-bot.py
@@ -0,0 +1,52 @@
+import cv2
+import requests
+import shutil
+import skimage.metrics
+import subprocess
+
+CURRENT_PATH = "current.jpeg"
+CANDIDATE_PATH = "candidate.jpeg"
+
+SIMILARITY_THRESHOLD = 0.9
+
+def post_image(path):
+  url = "https://gate.whapi.cloud/messages/image"
+  token = "TJxWS1xNvd7o30glG6k1fSzXsqgRPOhQ"
+
+  data = {
+    "to": "120363422200045729@g.us", # Board-bot-test group
+    "caption": "The latest image from Board-bot.",
+  }
+
+  files = {
+    "media": open(path, "rb")
+  }
+
+  headers = {
+    "authorization": f"Bearer {token}",
+  }
+
+  response = requests.post(url, data=data, files=files, headers=headers)
+
+  print(response.text)
+
+print(f"Capturing {CANDIDATE_PATH}")
+subprocess.run(["rpicam-jpeg", "--immediate", "--autofocus-on-capture", "--width", "1296", "--height", "972", "--output", CANDIDATE_PATH], check=True)
+
+current = cv2.imread(CURRENT_PATH)
+candidate = cv2.imread(CANDIDATE_PATH)
+
+current = cv2.cvtColor(current, cv2.COLOR_BGR2GRAY)
+candidate = cv2.cvtColor(candidate, cv2.COLOR_BGR2GRAY)
+
+print("Calculating structural similarity")
+similarity = skimage.metrics.structural_similarity(current, candidate)
+
+print(f"Structural similarity = {similarity}")
+
+if similarity < SIMILARITY_THRESHOLD:
+  print("Updating current image")
+  shutil.copyfile(CANDIDATE_PATH, CURRENT_PATH)
+  post_image(CURRENT_PATH)
+else:
+  print("No change")
