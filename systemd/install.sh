d96b9aa84e6b3ad4d9935efee86c53203ff7ab53
Ben Batt <benbatt@gmail.com>
2026-03-14 12:28:04 +1100

    Add systemd install script

---
+++ systemd/install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+mkdir -p ~/.config/systemd/user
+cp board-bot.service board-bot.timer ~/.config/systemd/user
+systemctl --user enable --now board-bot.timer
+systemctl --user daemon-reload
